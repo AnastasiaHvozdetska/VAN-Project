c013499f692457f2454eb77095b322d5ae1d4d45
AnastasiaHvozdetska <anastasia9297@gmail.com>
2018-04-03 15:24:47 +0300

    updated

--- build/js/libs.js
+++ build/js/libs.js
@@ -18,14 +18,39 @@
 
 $(document).ready(function () {
 	//slider
-	$('.slider').slick({
-		dots: true,
-		arrows: false,
-		slidesToShow: 3,
-		slidesToScroll: 3,
-		autoplay: true,
-		autoplaySpeed: 2000,
-	});
+$('.slider').slick({
+  dots: true,
+  arrows: false,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
 
 	$('.checkbox').on('click', function () {
 		$('.checkbox').css('background-color', '#303030');
@@ -44,6 +69,11 @@ $(document).ready(function () {
 		$('.left-menu-link').filter(function(){
 			return url.indexOf(this.href) != -1;
 		}).addClass('current-link');
+	});
+	$(function(){
+		$('#left-menu-button').on('click', function(){
+			$('.left-menu-wrapper').css('display', 'block');
+		});
 	});
 });
 
