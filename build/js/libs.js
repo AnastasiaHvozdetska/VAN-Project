90d92ea3b6d32b43ac6d317f48dcc2ef1b6dbff5
AnastasiaHvozdetska <anastasia9297@gmail.com>
2018-04-04 13:27:01 +0300

    updated adaptive

--- build/js/libs.js
+++ build/js/libs.js
@@ -18,39 +18,39 @@
 
 $(document).ready(function () {
 	//slider
-$('.slider').slick({
-  dots: true,
-  arrows: false,
-  infinite: false,
-  speed: 300,
-  slidesToShow: 3,
-  slidesToScroll: 3,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
-        dots: true
-      }
+	$('.slider').slick({
+		dots: true,
+		arrows: false,
+		infinite: false,
+		speed: 300,
+		slidesToShow: 3,
+		slidesToScroll: 3,
+		responsive: [
+			{
+				breakpoint: 1024,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 3,
+					infinite: true,
+					dots: true
+				}
     },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2
-      }
+			{
+				breakpoint: 600,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 2
+				}
     },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
+			{
+				breakpoint: 480,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1
+				}
     }
   ]
-});
+	});
 
 	$('.checkbox').on('click', function () {
 		$('.checkbox').css('background-color', '#303030');
@@ -58,30 +58,33 @@ $('.slider').slick({
 		$('label').css('color', '#fff');
 		$(this).find('label').css('color', '#303030');
 	});
-	
-	$(function(){
+
+	$(function () {
 		var heightMenu = $('.account-content').height();
 		$('.left-menu-wrapper').height(heightMenu);
 	});
-	
-	$(function(){
+
+	$(function () {
 		var url = document.location.toString();
-		$('.left-menu-link').filter(function(){
+		$('.left-menu-link').filter(function () {
 			return url.indexOf(this.href) != -1;
 		}).addClass('current-link');
 	});
-	$(function(){
-		$('#left-menu-button').on('click', function(){
+	$(function () {
+		$(document).on('click', '.open-menu', function () {
+			$(this).addClass('close-menu');
+			$(this).removeClass('open-menu');
 			$('.left-menu-wrapper').animate({
 				left: '0px'
 			}, 200);
+
 		});
-		$('.close-left-menu').on('click', function(){
+		$(document).on('click', '.close-menu', function () {
+			$(this).addClass('open-menu');
+			$(this).removeClass('close-menu');
 			$('.left-menu-wrapper').animate({
-				left: '-65%'
+				left: '-50%'
 			}, 200);
 		});
 	});
 });
-
-	
